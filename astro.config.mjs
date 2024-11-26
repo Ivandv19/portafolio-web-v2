c61d91e5e7b51aa8971741854189d6b37c2d0d57
ivandevI9 <ivangtx19@gmail.com>
2024-11-26 09:34:12 -0600

    fix: arregla claves de acceso a la api de Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  server: {
-    proxy: {
-      '/api': 'https://screenshotapi.net', // Ruta del proxy
-    },
-  },
+  
 });
