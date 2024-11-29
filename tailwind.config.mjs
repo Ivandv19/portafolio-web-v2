e4566a84ad7fb6e1130c40a21b0d80718172a5bd
ivandevI9 <ivangtx19@gmail.com>
2024-11-29 12:57:44 -0600

    style(header): agrega barra roja

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ const { addDynamicIconSelectors } = require("@iconify/tailwind");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        neon: "0 0 15px rgba(255, 0, 0, 0.6)", // sombra personalizada
+      },
+    },
   },
   plugins: [addDynamicIconSelectors()],
 };
