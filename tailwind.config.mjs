42e21d333e9e7cab5674073d9e8ef423884ae11a
ivandevI9 <ivangtx19@gmail.com>
2024-11-29 13:50:14 -0600

    style: agrega fuente Montserrat por defecto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const { addDynamicIconSelectors } = require("@iconify/tailwind");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       boxShadow: {
         neon: "0 0 15px rgba(255, 0, 0, 0.6)", // sombra personalizada
+      },
+      fontFamily: {
+        // Define tu fuente aquí
+        sans: ['"Montserrat"','"Roboto"', '"Helvetica Neue"', 'Arial', 'sans-serif'],
+        serif: ['"Merriweather"', 'serif'],
       },
     },
   },
