95749014e7603cecb1c65f4599c87e35e96597c4
ivandevI9 <ivangtx19@gmail.com>
2024-12-02 11:30:52 -0600

    fix(index): arregla sombra del header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,8 +5,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'border-color': '#FF0000',
+      },
+
       boxShadow: {
-        neon: "0 0 15px rgba(255, 0, 0, 0.6)", // sombra personalizada
+        custom: '0px -3px 15px 1px #FF0000',
       },
       fontFamily: {
         // Define tu fuente aquí
